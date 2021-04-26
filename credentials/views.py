4e0c31d091a078561c04c5bfaa7f78eed62e6449
Siegfred Samson <siegfredsamson@gmail.com>
2021-04-26 09:56:34 +0100

    views.py added

---
+++ credentials/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.views.generic import CreateView
+from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm
+from django.contrib.auth.views import PasswordChangeView
+from django.urls import reverse_lazy
+
+
+# Create your views here.
+class BlogRegisterView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'registration/register.html'
+    success_url = reverse_lazy('login')
+
+
+class PasswordsChangeView(PasswordChangeView):
+    form_class = PasswordChangeForm
+    success_url = reverse_lazy('password_success')
+
+
+def password_success(request):
+    return render(request, 'registration/password_success.html', {} )

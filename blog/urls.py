4538ba96c3335fc100bad7a5358bdff225ec2e73
Siegfred Samson <siegfredsamson@gmail.com>
2021-05-16 10:33:07 +0100

    path for BlogCommentView added in urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView, BlogDeleteView
+from .views import BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView, BlogDeleteView, BlogCommentView
 
 urlpatterns = [
     path('post/<int:pk>/delete', BlogDeleteView.as_view(), name='post_delete'),
@@ -7,6 +7,6 @@ urlpatterns = [
     path('post/new/', BlogCreateView.as_view(), name='post_new'),
     path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
     path('', BlogListView.as_view(), name='home'),
-
+    path('post/<int:pk>/comment/', BlogCommentView.as_view(), name='post_comment'),
 ]
 
